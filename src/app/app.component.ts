62d157d88ffc8533a053f0c485ccf486484e548d
Krister <krister.torsvik@gmail.com>
2017-04-25 08:53:03 +0200

    subject/switchmap/interval in use

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs/Rx';
+import { Subject } from 'rxjs/Subject';
+import 'rxjs/Rx';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  // A subject is an observer and an observable which is called by the start button with start$.next()
+  start$ = new Subject()
+
+  //standard interval observable which ticks every 1 second
+  interval$ = Observable.interval(1000);
+
+  //startInterval tells the start$ subject to start the interval when it is getting a click
+  startInterval$ = this.start$
+    //switchMap switches to the inner observable after the first observable has fired,
+    //and only takes values from the most recently projected inner Observable
+    .switchMap((event) => this.interval$);
+
+  constructor() {
+    //subscribing to the startInterval$ to make it fire
+    this.startInterval$
+      .subscribe((x) => console.log(x));
+  }
 }
